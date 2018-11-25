966c5f517117f290c853f6d47fbd45c37a5c4580
Sunhwan Jo <sunhwanj@gmail.com>
2018-11-25 08:23:57 -0800

    add templates

--- manage.py
+++ manage.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask import render_template
+
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template('hello.html')
